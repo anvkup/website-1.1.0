01fd8dbbd31147659b7593e4ed0729378635703a
Anvesh Kumar <anvkup2002@gmail.com>
2022-01-27 14:47:28 +0530

    Please enter the commit message for your changes. removed node modules
     removes node modules

--- index.js
+++ index.js
@@ -6,22 +6,27 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'views/index.html'))
+  res.end();
 })
 
 app.get('/about', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'views/about.html'))
+  res.end();
 })
 
 app.get('/bookings', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'views/bookings.html'))
+  res.end();
 })
 
 app.get('/contact', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'views/contact.html'))
+  res.end();
 })
 
 app.all('*', (req, res) => {
   res.status(404).sendFile(path.resolve(__dirname, 'views/404.html'))
+  res.end();
 })
 
 app.listen(3000); {
